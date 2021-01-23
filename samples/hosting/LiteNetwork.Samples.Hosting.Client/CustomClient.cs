8ff3a59dd404ff00507e7d7090287c922820a35f
Emilio Daniel Sánchez Montes <extrez10@gmail.com>
2021-01-23 00:21:57 -0600

    Add hosting extensions

---
+++ samples/hosting/LiteNetwork.Samples.Hosting.Client/CustomClient.cs
@@ -0,0 +1,46 @@
+﻿using LiteNetwork.Client;
+using LiteNetwork.Protocol.Abstractions;
+using System;
+using System.Threading.Tasks;
+
+namespace LiteNetwork.Samples.Hosting.Client
+{
+    public interface ICustomClient
+    {
+        void DoSomething();
+    }
+
+    public class CustomClient : LiteClient, ICustomClient
+    {
+        public CustomClient(LiteClientOptions options, IServiceProvider serviceProvider = null)
+            : base(options, serviceProvider)
+        {
+        }
+
+        public override Task HandleMessageAsync(ILitePacketStream incomingPacketStream)
+        {
+            string receivedMessage = incomingPacketStream.ReadString();
+
+            Console.WriteLine($"Received message: {receivedMessage}");
+
+            return base.HandleMessageAsync(incomingPacketStream);
+        }
+
+        protected override void OnConnected()
+        {
+            Console.WriteLine($"Client connected to {Options.Host}:{Options.Port}");
+            base.OnConnected();
+        }
+
+        protected override void OnDisconnected()
+        {
+            Console.WriteLine($"Client disconnected.");
+            base.OnDisconnected();
+        }
+
+        public void DoSomething()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/LiteNetwork.Client/Hosting/LiteClientHostedService.cs
@@ -0,0 +1,36 @@
+﻿using LiteNetwork.Client.Abstractions;
+using Microsoft.Extensions.Hosting;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace LiteNetwork.Client.Hosting
+{
+    /// <summary>
+    /// Defines a basic <see cref="IHostedService"/> to use with <see cref="LiteClient"/>
+    /// </summary>
+    internal class LiteClientHostedService : IHostedService
+    {
+        private readonly ILiteClient _client;
+
+        /// <summary>
+        /// Creates a new <see cref="LiteClientHostedService"/> with the given server.
+        /// </summary>
+        /// <param name="client">Client to host.</param>
+        public LiteClientHostedService(ILiteClient client)
+        {
+            _client = client;
+        }
+
+        /// <inheritdoc />
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            return _client.ConnectAsync();
+        }
+
+        /// <inheritdoc />
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            return _client.DisconnectAsync();
+        }
+    }
+}
